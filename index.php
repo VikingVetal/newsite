0d69b2e41b2cf3b5885abe9ad8dfc34070a626d5
VikingVetal <67791758+VikingVetal@users.noreply.github.com>
2021-07-17 09:30:06 +0300

    Add files

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+//echo "Hallo world" . PHP_EOL;
+//echo mb_strlen (" Привет");
+//echo strlen ("Hallo world");
+//echo count(explode(" ","good morning"));
+//var_export(explode(" ","good morning"));
+//var_dump(explode(" ","good morning"));
+
+ ?>
